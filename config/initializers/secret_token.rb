299a5dcd9c1264306c8a7d035e0154ae5a7a1ae7
Grant Hutchins <nertzy@gmail.com>
2012-07-15 17:23:56 -0400

    rails new store_example

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoreExample::Application.config.secret_token = 'd7be6e3f54ec08779098bf3b3fc87eb38aeae36495dcd7df986d6c2eb1c861a4545ca1c24d88d68ae2dd80e6230c0604247f1531d1454cbc6bf656d3d1e443b0'
