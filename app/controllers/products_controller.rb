d39596d64a664df8e34817d27fc7a0be18168dd3
Grant Hutchins <nertzy@gmail.com>
2012-07-15 18:34:50 -0400

    Add basic scaffold for creating products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,23 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.scoped
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(params[:product])
+
+    if @product.valid?
+      redirect_to @product
+    else
+      render :new
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
   end
 end
