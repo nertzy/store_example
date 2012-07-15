991cc266f39911836af2c420581014a2b779d854
Grant Hutchins <nertzy@gmail.com>
2012-07-15 18:20:51 -0400

    Add product model

---
+++ db/migrate/20120715221220_create_products.rb
@@ -0,0 +1,11 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :photo_url
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
