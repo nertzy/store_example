086901464c2391184724261d3d79b87605c6d1b4
Grant Hutchins <nertzy@gmail.com>
2012-07-16 22:04:20 -0400

    Add some cheesy logic to the controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,6 +3,15 @@ class SearchesController < ApplicationController
   end
 
   def show
-    @products = Product.where("name LIKE ?", "%#{params[:query]}%")
+    if params[:query].blank?
+      @missing_query = true
+      @products = []
+    else
+      @products = Product.where("name LIKE ?", "%#{params[:query]}%")
+    end
+
+    if params[:query] =~ /shoes/i
+      flash.now.alert = "Sorry, we don't sell shoes anymore"
+    end
   end
 end
