97284b64175668350f3805145bbff1aefb9faece
Grant Hutchins <nertzy@gmail.com>
2012-07-16 21:20:56 -0400

    Generate fewer things

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,11 @@ module StoreExample
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.test_framework false
+      g.helper false
+      g.assets false
+    end
   end
 end
